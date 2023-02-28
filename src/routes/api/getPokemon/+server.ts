a539b82e79b0070347b41b81840521fbfa9dd17f
code-withAshish <abhushan064@gmail.com>
2023-02-28 12:15:44 +0530

    fix devDependecies issue

--- src/routes/api/getPokemon/+server.ts
+++ src/routes/api/getPokemon/+server.ts
@@ -1,5 +1,5 @@
 
-import { error, json as sveltejson } from '@sveltejs/kit';
+import { json as sveltejson } from '@sveltejs/kit';
 import { prisma } from "$lib/db"
 import type { RequestHandler } from './$types';
 
@@ -9,9 +9,7 @@ export const GET = (async ({ url }) => {
 
     const id1 = Number(url.searchParams.get('id1'))
     const id2 = Number(url.searchParams.get('id2'))
-    if ((id1 <= 0 || id1 > 1008) || (id2 <= 0 || id2 > 1008) || (id1 === id2)) {
-        throw error(400, "Bad query params")
-    }
+
     const pokemons = await prisma.pokemon.findMany({ where: { id: { in: [id1, id2] } } })
     return sveltejson({
         poke1: { id: pokemons[0].id, name: pokemons[0].name, img: pokemons[0].spriteUrl },
