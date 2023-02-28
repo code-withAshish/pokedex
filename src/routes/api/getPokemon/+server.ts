c424440e8326d8965feed24c63f4addbf33d65aa
code-withAshish <abhushan064@gmail.com>
2023-02-28 13:08:50 +0530

    fix prisma query

--- src/routes/api/getPokemon/+server.ts
+++ src/routes/api/getPokemon/+server.ts
@@ -10,7 +10,7 @@ export const GET = (async ({ url }) => {
     const id1 = Number(url.searchParams.get('id1'))
     const id2 = Number(url.searchParams.get('id2'))
 
-    const pokemons = await prisma.pokemon.findMany({ where: { AND: [{ id: id1 }, { id: id2 }] } })
+    const pokemons = await prisma.pokemon.findMany({ where: { id: { in: [id1, id2] } } })
     return sveltejson({
         poke1: { id: pokemons[0].id, name: pokemons[0].name, img: pokemons[0].spriteUrl },
         poke2: { id: pokemons[1].id, name: pokemons[1].name, img: pokemons[1].spriteUrl }
